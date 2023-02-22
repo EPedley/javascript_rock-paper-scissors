fe6be4ee4fa06be10ea6b644b73faa8f1ad94111
EPedley <lizzypedley@gmail.com>
2023-02-22 17:30:42 +0000

    Completed project

--- javascript.js
+++ javascript.js
@@ -1,48 +1,6 @@
-//function to decide who the winner is
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === "SCISSORS") {
-        if (computerSelection === "PAPER") {
-            const element = document.getElementById("score_text");
-            element.innerHTML = "YOU WIN";
-        }
-        else if (computerSelection === "ROCK") {
-            const element = document.getElementById("score_text");
-            element.innerHTML = "YOU LOSE";
-        }
-        else {
-            const element = document.getElementById("score_text");
-            element.innerHTML = "IT'S A TIE";
-        }
-    }
-    else if (playerSelection === "PAPER") {
-        if (computerSelection === "ROCK") {
-            const element = document.getElementById("score_text");
-            element.innerHTML = "YOU WIN";
-        }
-        else if (computerSelection === "SCISSORS") {
-            const element = document.getElementById("score_text");
-            element.innerHTML = "YOU LOSE";
-        }
-        else {
-            const element = document.getElementById("score_text");
-            element.innerHTML = "IT'S A TIE";
-        }
-    }
-    else {
-        if (computerSelection === "SCISSORS") {
-            const element = document.getElementById("score_text");
-            element.innerHTML = "YOU WIN";
-        }
-        else if (computerSelection === "PAPER") {
-            const element = document.getElementById("score_text");
-            element.innerHTML = "YOU LOSE";
-        }
-        else {
-            const element = document.getElementById("score_text");
-            element.innerHTML = "IT'S A TIE";
-        }
-    }
-}
+const buttons = document.querySelectorAll('button');
+let player_counter = 0;
+let computer_counter = 0;
 
 //function to change the image in the results element depending on the computer's choice
 function showComputerChoice(computerSelection) {
@@ -76,14 +34,9 @@ function showPlayerChoice(playerSelection) {
     }  
 }
 
-function getPlayerChoice() {
-    return "SCISSORS";
-}
-
 //function to get a random selection from the computer
 function getComputerChoice() {
     let random = Math.floor(Math.random() * 3);
-    console.log(random);
     if (random === 0) {
         choice = "ROCK";
         return choice;
@@ -98,46 +51,98 @@ function getComputerChoice() {
     }
 }
 
-//function to select ROCK for the user and call the getComputerChoice, showPlayerChoice and showComputerChoice functions
-function rock_click() {
+//listen to the buttons
+buttons.forEach(button =>{
+    button.addEventListener('click', function(){
+    game(button.value)
+})
+})
+
+function playRound(playerSelection) {
+    //call the function that decides the computer's weapon
     let computerSelection = getComputerChoice();
-    playerSelection = "ROCK";
-    showPlayerChoice(playerSelection);
+
+    //call the functions that show the users the weapons used
     showComputerChoice(computerSelection);
-}
-
-//function to select PAPER for the user and call the getComputerChoice, showPlayerChoice and showComputerChoice functions
-function paper_click() {
-    let computerSelection = getComputerChoice();
-    playerSelection = "PAPER";
     showPlayerChoice(playerSelection);
-    showComputerChoice(computerSelection);
+
+    let log = "";
+
+        if (playerSelection === "SCISSORS") {
+            if (computerSelection === "PAPER") {
+                const element = document.getElementById("score_text");
+                element.innerHTML = "YOU WIN";
+                log = "PLAYER";
+            }
+            else if (computerSelection === "ROCK") {
+                const element = document.getElementById("score_text");
+                element.innerHTML = "YOU LOSE";
+                log = "COMPUTER";
+            }
+            else {
+                const element = document.getElementById("score_text");
+                element.innerHTML = "IT'S A TIE";
+                log = "TIE";
+            }
+        }
+        else if (playerSelection === "PAPER") {
+            if (computerSelection === "ROCK") {
+                const element = document.getElementById("score_text");
+                element.innerHTML = "YOU WIN";
+                log = "PLAYER";
+            }
+            else if (computerSelection === "SCISSORS") {
+                const element = document.getElementById("score_text");
+                element.innerHTML = "YOU LOSE";
+                log = "COMPUTER";
+            }
+            else {
+                const element = document.getElementById("score_text");
+                element.innerHTML = "IT'S A TIE";
+                log = "TIE";
+            }
+        }
+        else {
+            if (computerSelection === "SCISSORS") {
+                const element = document.getElementById("score_text");
+                element.innerHTML = "YOU WIN";
+                log = "PLAYER";
+            }
+            else if (computerSelection === "PAPER") {
+                const element = document.getElementById("score_text");
+                element.innerHTML = "YOU LOSE";
+                log = "COMPUTER";
+            }
+            else {
+                const element = document.getElementById("score_text");
+                element.innerHTML = "IT'S A TIE";
+                log = "TIE";
+            }
+        }
+        return log;
+    }
+
+function game(playerSelection) {
+    const player_counter_element = document.getElementById("player_counter");
+    player_counter_element.innerHTML = player_counter;
+
+    const computer_counter_element = document.getElementById("computer_counter");
+    computer_counter_element.innerHTML = computer_counter;
+
+    winner = playRound(playerSelection);
+
+    if (winner === "PLAYER") {
+        ++player_counter;
+        player_counter_element.innerHTML = player_counter;
+    }
+    else if (winner === "COMPUTER") {
+        ++computer_counter;
+        computer_counter_element.innerHTML = computer_counter;
+    }
+    if (player_counter === 5) {
+        location.href = './you_win.html';
+    }
+    else if (computer_counter === 5) {
+        location.href = './you_lose.html';
+    }
 }
-
-//function to select SCISSORS for the user and call the getComputerChoice, showPlayerChoice and showComputerChoice functions
-function scissors_click() {
-    let computerSelection = getComputerChoice();
-    playerSelection = "SCISSORS";
-    showPlayerChoice(playerSelection);
-    showComputerChoice(computerSelection);
-}
-
-//declare my playerSelection and computerSelection variables but don't populate them
-let playerSelection;
-let computerSelection;
-
-//listen to the ROCK button
-const rock = document.getElementById("rock");
-rock.addEventListener("click", rock_click);
-
-//listen to the PAPER button
-const paper = document.getElementById("paper");
-paper.addEventListener("click", paper_click);
-
-//listen to the SCISSORS button
-const scissors = document.getElementById("scissors");
-scissors.addEventListener("click", scissors_click);
-
-
-
-playRound(playerSelection, computerSelection);
